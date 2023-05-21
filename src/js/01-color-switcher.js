0ec314005f11d3333e4cfc60b32160048ca1f0e8
Oleksandr Mysan <werasik228@gmail.com>
2023-05-21 15:07:25 +0300

    last

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let timer;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+startButton.addEventListener('click', function() {
+  startButton.disabled = true;
+  timer = setInterval(function() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 500);
+});
+
+stopButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  clearInterval(timer);
+});
